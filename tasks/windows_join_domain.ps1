1a4ac8d61c2dbdf230d5d35ae35576187f5cafd2
Mike Smith <mike.smith@puppet.com>
2019-01-06 17:31:23 -0800

    Update windows_join_domain.ps1

--- tasks/windows_join_domain.ps1
+++ tasks/windows_join_domain.ps1
@@ -1,8 +1,8 @@
 # Puppet Task Name: windows_domain_join
 
-param ($domain)
-param ($username)
-param ($password)
+#param ($domain)
+#param ($username)
+#param ($password)
 
 $cred = New-Object System.Management.Automation.PsCredential("$domain\\$username", (ConvertTo-SecureString "$password" -AsPlainText -Force))
 Add-Computer -DomainName "$domain" -Credential $cred
